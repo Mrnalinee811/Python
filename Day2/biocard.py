bc7ec0d65119e2461afa623923bd09c4d466bfe7
Mrnalinee811 <Mrnalinee123@gmail.com>
2026-05-22 13:44:17 -0400

    fourth commit

---
+++ Day2/biocard.py
@@ -0,0 +1,11 @@
+name = 'Mrnalinee'
+age = 28
+city = 'Boston'
+hobby = 'cooking'
+dream_job = 'data engineer'
+
+print('Name: ' + name)
+print('Age: ' + str(age))
+print('City: ' + city)
+print('Hobby: ' + hobby)
+print('Dream Job: ' + dream_job)
