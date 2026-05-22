0971457008dbe1933df8afca0c176800d1f688d0
Mrnalinee811 <Mrnalinee123@gmail.com>
2026-05-22 14:06:37 -0400

    git update

---
+++ Day4/conditions.py
@@ -0,0 +1,8 @@
+#If Elif Else
+age = int(input('Enter age: '))
+if age >= 18:
+    print('You are an adult')
+elif age >= 13:
+    print('You are a teenager')
+else:
+    print('You are a child')
---
+++ Day4/grade.py
@@ -0,0 +1,12 @@
+score = int(input('Enter your score: '))
+
+if score >= 90:
+    print('Grade: A - Excellent!')
+elif score >= 80:
+    print('Grade: B - Good job!')
+elif score >= 70:
+    print('Grade: C - Keep trying!')
+elif score >= 60:
+    print('Grade: D - Need improvement!')
+else:
+    print('Grade: F - Please study harder!')
