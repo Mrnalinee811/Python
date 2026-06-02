3b9e04f75c8a0fca01919f3ceef7d67c6646ffe2
Mrnalinee811 <Mrnalinee123@gmail.com>
2026-06-02 18:01:02 -0400

    Modified program4

--- Day2/program4.py
+++ Day2/program4.py
@@ -1,3 +1,6 @@
 #WAP to find the occurrence of '$' in a String.
-str ="my name is Mrnalinee. My name is divya. My name is shreya"
-print(str.count("name"))
+# str ="my name is Mrnalinee . My name is divya. My name is shreya"
+# print(str.count("name"))
+
+str ="my name is Mrnalinee $. My name is divya $$$$$. My name is shreya"
+print(str.count("$"))
