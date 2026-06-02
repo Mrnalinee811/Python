0c02b4c11c812117250302645dcb971bd3bf7ed3
Mrnalinee811 <Mrnalinee123@gmail.com>
2026-06-02 17:58:34 -0400

    Add programs: print name length and find occurrence of $ in string

---
+++ Day2/Program3.py
@@ -0,0 +1,5 @@
+#WAP to input user's first name & print its length.
+name =input("Enter the name:")
+print("your name is :",len(name))
+
+#print(var2)
---
+++ Day2/program4.py
@@ -0,0 +1,3 @@
+#WAP to find the occurrence of '$' in a String.
+str ="my name is Mrnalinee. My name is divya. My name is shreya"
+print(str.count("name"))
